2ba8164b7e93a4740854d337321a474598e786dd
zahirshaikh <63846861+zahirshaikh@users.noreply.github.com>
2020-05-27 17:01:25 +0530

    Update handler.php

--- contact form/handler.php
+++ contact form/handler.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('zahirshaikh@gmail.com'); // ← Your email here
+$pp->sendEmailTo('zahirshaikh026@gmail.com'); // ← Your email here
 
 echo $pp->process($_POST);
